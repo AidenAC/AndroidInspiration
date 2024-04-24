a619e48aa959f77f0f59766fc5a10d5310375e92
AidenAC <aidenc@umich.edu>
2024-04-24 17:06:16 -0400

    get quote from ZenQuotes API

--- app/src/main/java/com/cis436/project4/FirstFragment.kt
+++ app/src/main/java/com/cis436/project4/FirstFragment.kt
@@ -1,22 +1,28 @@
 package com.cis436.project4
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.cis436.project4.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +30,13 @@ class FirstFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        binding.btGetQuote.setOnClickListener {
+            getQuote()
+        }
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,5 +46,25 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getQuote() {
+        val url = "https://zenquotes.io/api" + "/random"
+        val queue = Volley.newRequestQueue(this.context)
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            { response ->
+                Log.i("Get Quote", response.toString())
+                viewModel.setQuote(response.toString())
+                binding.tvQuote.text = viewModel.getQuote()
+            },
+            {
+                Log.i("Get Quote", "That did not work!")
+            }
+        )
+
+        queue.add(stringRequest)
     }
 }
--- app/src/main/java/com/cis436/project4/MainViewModel.kt
+++ app/src/main/java/com/cis436/project4/MainViewModel.kt
@@ -1,14 +1,17 @@
 package com.cis436.project4
 
 import androidx.lifecycle.ViewModel
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainViewModel : ViewModel() {
-    private lateinit var quote : String
+    private lateinit var quote : JSONObject
 
-    fun setQuote(quote : String) {
-        this.quote = quote
+    fun setQuote(json : String) {
+        val array = JSONArray(json)
+        quote = array.getJSONObject(0)
     }
     fun getQuote() : String {
-        return quote
+        return quote.get("q").toString()
     }
 }
